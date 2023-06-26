42f8263ecaea509c3ee400920321185516d020ec
Darren Duan <tsdarren@gmail.com>
2023-06-26 16:38:30 +1000

    added buttons for word guessing game

--- src/components/WordGuess/WordGuess.js
+++ src/components/WordGuess/WordGuess.js
@@ -1,11 +1,16 @@
 import Chip from './components/Chip'
+import Button from '../Button/Button'
 const WordGuess = () => {
   const CHIP_INDEX = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11, 12]
   return (
-    <div className="main_container WordGuess_main_container ">
-    {CHIP_INDEX.map((index) => (<Chip key={index} index={index} />)) 
-    }
-  </div>
+    <div>
+      <div className="main_container WordGuess_main_container ">
+        {CHIP_INDEX.map((index) => (<Chip key={index} index={index} />)) }
+      </div>
+      <div className='low_container'>
+        <Button>Start</Button>
+      </div>
+    </div>
   )
 }
 export default WordGuess;
