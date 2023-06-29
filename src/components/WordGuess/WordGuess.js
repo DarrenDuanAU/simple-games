a1a731a87f3ac4eb6a5c2c62b6decd88e34d5fb8
Darren Duan <tsdarren@gmail.com>
2023-06-29 10:58:07 +1000

    add welcome word for word guess game

--- src/components/WordGuess/WordGuess.js
+++ src/components/WordGuess/WordGuess.js
@@ -1,13 +1,15 @@
 import Chip from './components/Chip'
 import Button from '../Button/Button'
 import { wordStrs } from '../../data/wordStrs'
+import { useState } from 'react'
 
 const WordGuess = () => {
   const CHIP_INDEX = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11]
+  const [currentWord, setCurrentWord] = useState("click button");
   return (
     <div>
       <div className="main_container WordGuess_main_container ">
-        {CHIP_INDEX.map((index) => (<Chip key={index} index={wordStrs[1][index]} />)) }
+        {CHIP_INDEX.map((index) => (<Chip key={index} index={currentWord[index]} />)) }
       </div>
       <div className='low_container'>
         <Button>Start</Button>
