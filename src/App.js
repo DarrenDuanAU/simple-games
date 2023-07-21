c71816f72cb44605a3253f7a66b887afcdea9e33
Darren Duan <tsdarren@gmail.com>
2023-07-21 13:44:30 +1000

    move sliding puzzle function from App.js to SlidingPuzzle folder

--- src/App.js
+++ src/App.js
@@ -6,92 +6,25 @@ import Tetris from './components/Tetris';
 import Home from './components/Home';
 import Header from './components/Header';
 import Footer from './components/Footer'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import React from 'react'
-import { shuffleArray, swapTwoElementsInArray } from './utils/commonFunctions';
-
-
-const PUZZLE_CLICK_MAP = [
-  { click: 0, check: [1, 3] },
-  { click: 1, check: [0, 2, 4] },
-  { click: 2, check: [1, 5] },
-  { click: 3, check: [0, 4, 6] },
-  { click: 4, check: [1, 3, 5, 7] },
-  { click: 5, check: [2, 4, 8] },
-  { click: 6, check: [3, 7] },
-  { click: 7, check: [4, 6, 8] },
-  { click: 8, check: [5, 7] }
-]
 
 function App() {
   const [score, setScore] = useState(0);
   const [currentPage, setCurrentPage] = useState("Home")
-  const [puzzleImageIds, SetPuzzleImageIds] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
-  const [puzzleGameStart, setPuzzleGameStart] = useState(false);
 
   const ButtonClickHandler = (page) => {
     setCurrentPage(page)
   }
 
-  // functions for Sliding Puzzles
-  const startGameSlidingPuzzle = () => {
-    if(puzzleGameStart === false) {
-      SetPuzzleImageIds(shuffleArray([1,2,3,4,5,6,7,8,0]));
-      setPuzzleGameStart(true);
-    }
-  }
-
-  useEffect(() => {
-    if ( puzzleGameStart === true &&
-      JSON.stringify(puzzleImageIds) === JSON.stringify([0,1,2,3,4,5,6,7,8]) ) {
-      setScore( prevState => prevState + 1 );
-      alert('Sliding Puzzle win!');
-    }
-  },[puzzleImageIds, puzzleGameStart]);
-
-  const resetGameSlidingPuzzle = () => {
-    if(puzzleGameStart === true){
-      setPuzzleGameStart(false)
-      SetPuzzleImageIds([0,1,2,3,4,5,6,7,8]);
-    }
-  }
-
-  const solveGameSlidingPuzzle = () => {
-    if (puzzleGameStart === true) {
-      SetPuzzleImageIds([0,1,2,3,4,5,6,7,8]);
-    }
-  }
-
-  const switchImage = (locationId) => {
-    const locationChecks = PUZZLE_CLICK_MAP.find((item) => item.click === locationId)?.check 
-    for( let i=0 ; i<locationChecks.length ; i++ ) {
-      if( puzzleImageIds[locationChecks[i]] === 8 ) {
-        const targetLocationId = locationChecks[i]
-        SetPuzzleImageIds((prevState) => {
-          return swapTwoElementsInArray([...prevState], locationId, targetLocationId)
-        });
-      }
-    }
-  }
 
   return (
     <>
       <Header onHeaderClick={ButtonClickHandler}/>
       <div className='page'>          
         {(currentPage === "Home") && <Home score={score}/>}
-        {(currentPage === "Sliding Puzzle") && 
-          <SlidingPuzzle 
-            gameStarted={puzzleGameStart}
-            startTheGame={startGameSlidingPuzzle} 
-            resetTheGame={resetGameSlidingPuzzle}
-            solveTheGame={solveGameSlidingPuzzle}
-            onImageClick={switchImage}
-            imageIds={puzzleImageIds} 
-          />}
-        {(currentPage === "Tetris") && 
-        <Tetris 
-
-        />}
+        {(currentPage === "Sliding Puzzle") && <SlidingPuzzle />}
+        {(currentPage === "Tetris") && <Tetris />}
         {(currentPage === "Word Guess") && <WordGuess />}
       </div>
       <Footer />
--- src/components/SlidingPuzzle/SlidingPuzzle.js
+++ src/components/SlidingPuzzle/SlidingPuzzle.js
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react'
 import Cell from './components/Cell'
 import Button from '../Button'
+import { shuffleArray, swapTwoElementsInArray } from '../../utils/commonFunctions'
 import { chip0, chip1, chip2, chip3, chip4, chip5, 
   chip6, chip7, chip8} from '../../data/shrek'
 
@@ -8,22 +10,70 @@ const SHREK_PIECES = [
   chip6, chip7, chip8
 ]
 
-const SlidingPuzzle = ({
-  gameStarted,
-  startTheGame,
-  resetTheGame,
-  solveTheGame,
-  onImageClick,
-  imageIds,
-}) => {
+const PUZZLE_CLICK_MAP = [
+  { click: 0, check: [1, 3] },
+  { click: 1, check: [0, 2, 4] },
+  { click: 2, check: [1, 5] },
+  { click: 3, check: [0, 4, 6] },
+  { click: 4, check: [1, 3, 5, 7] },
+  { click: 5, check: [2, 4, 8] },
+  { click: 6, check: [3, 7] },
+  { click: 7, check: [4, 6, 8] },
+  { click: 8, check: [5, 7] }
+]
+
+const SlidingPuzzle = () => {
+  const [imageIds, SetImageIds] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+  const [gameStarted, setGameStarted] = useState(false);
+
+  const startTheGame = () => {
+    if(gameStarted === false) {
+      console.log('start the game')
+      SetImageIds(shuffleArray([1,2,3,4,5,6,7,8,0]));
+      setGameStarted(true);
+    }
+  }
+
+  useEffect(() => {
+    if ( gameStarted === true &&
+      JSON.stringify(imageIds) === JSON.stringify([0,1,2,3,4,5,6,7,8]) ) {
+      alert('Sliding Puzzle win!');
+    }
+  },[imageIds, gameStarted]);
+
+  const resetTheGame = () => {
+    if(gameStarted === true){
+      setGameStarted(false)
+      SetImageIds([0,1,2,3,4,5,6,7,8]);
+    }
+  }
+
+  const solveTheGame= () => {
+    if (gameStarted=== true) {
+      SetImageIds([0,1,2,3,4,5,6,7,8]);
+    }
+  }
+
+  const switchImage = (locationId) => {
+    const locationChecks = PUZZLE_CLICK_MAP.find((item) => item.click === locationId)?.check 
+    for( let i=0 ; i<locationChecks.length ; i++ ) {
+      if( imageIds[locationChecks[i]] === 8 ) {
+        const targetLocationId = locationChecks[i]
+        SetImageIds((prevState) => {
+          return swapTwoElementsInArray([...prevState], locationId, targetLocationId)
+        });
+      }
+    }
+  }
 
   return(
     <div>
       <div className="main_container SlidingPuzzle_main_container">
         {imageIds.map((imageId,index) => 
-          (<Cell key={index} 
+          (<Cell 
+            key={index} 
             piece={SHREK_PIECES[imageId]}
-            onClick = {() => onImageClick(index)}
+            onClick = {() => switchImage(index)}
             />) ) 
         }
       </div>
@@ -32,7 +82,7 @@ const SlidingPuzzle = ({
           ? 
           <Button onClick={startTheGame}>Start</Button> 
           : 
-          <Button onClick={resetTheGame}>Reset</Button>}    
+          <Button onClick={resetTheGame}>Reset</Button>}     
         <Button onClick={solveTheGame}>Solve</Button>
       </div>
     </div>
