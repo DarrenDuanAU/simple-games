f7f7ad26ad5bd08ee90069027c07144d8c49dbe9
Darren Duan <tsdarren@gmail.com>
2023-07-23 23:01:02 +1000

    add style to the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
+import './style.css'
 const Footer = () => (
   <div className="footer">
-
+    <p className='copy-right'>© 2023 SupperCell, Inc. All rights reserved.</p>
   </div>
 )
 export default Footer;
